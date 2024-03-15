1a0a4845886c5af42627a087b6dc4ea6d6ac046f
Victor <47047497+173716414@users.noreply.github.com>
2024-03-15 10:20:22 +0800

    请求处理器，请求和响应类

---
+++ victor-rpc-easy/src/main/java/com/victor/vicrpc/model/RpcResponse.java
@@ -0,0 +1,46 @@
+package com.victor.vicrpc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/*
+ *Author：Victor_htq
+ *Package：com.victor.vicrpc.model
+ *Project：victor-rpc
+ *name：RpcResponse
+ *Date：2024/3/15  10:17
+ *Filename：RpcResponse
+ */
+
+/**
+ * RPC 响应
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RpcResponse implements Serializable {
+    /**
+     * 响应数据
+     */
+    private Object data;
+
+    /**
+     * 响应数据类型（预留）
+     */
+    private Class<?> dataType;
+
+    /**
+     * 响应信息
+     */
+    private String message;
+
+    /**
+     * 异常信息
+     */
+    private Exception exception;
+}
