4cbd16af1a50c223f0146341b2fe4e1b0f654cee
Victor <47047497+173716414@users.noreply.github.com>
2024-03-15 11:38:01 +0800

    静态代理

--- example-provider/src/main/java/com/victor/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/victor/example/provider/EasyProviderExample.java
@@ -1,5 +1,6 @@
 package com.victor.example.provider;
 
+import com.victor.example.common.service.UserService;
 import com.victor.vicrpc.registry.LocalRegistry;
 import com.victor.vicrpc.server.VertxHttpServer;
 
@@ -15,7 +16,7 @@ public class EasyProviderExample {
 
     public static void main(String[] args) {
 
-        LocalRegistry.register(UserServiceImpl.class.getName(), UserServiceImpl.class);
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
     //     提供web服务
         VertxHttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
