15ff6d23202f8bfb871379d228d3f3ad414ffcfd
Victor <47047497+173716414@users.noreply.github.com>
2024-03-28 15:51:27 +0800

    服务节点下线

--- victor-rpc-core/src/main/java/com/victor/vicrpc/RpcApplication.java
+++ victor-rpc-core/src/main/java/com/victor/vicrpc/RpcApplication.java
@@ -29,11 +29,14 @@ public class RpcApplication {
     public static void init(RpcConfig newRpcConfig) {
         rpcConfig = newRpcConfig;
         log.info("rpc init, config = {}", newRpcConfig.toString());
-
+        // 注册中西你初始化
         RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
         Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
         registry.init(registryConfig);
         log.info("registry init, config = {}", registryConfig);
+
+        // 创建并注册Shutdown Hook， JVM退出时执行操作
+        Runtime.getRuntime().addShutdownHook(new Thread(registry::destory));
     }
 
     public static void init() {
