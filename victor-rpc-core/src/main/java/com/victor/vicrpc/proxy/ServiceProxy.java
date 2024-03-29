00295f73a79af4df464bc486a83233f276278f1f
Victor <47047497+173716414@users.noreply.github.com>
2024-03-29 16:07:01 +0800

    修改服务消费者请求发送代码

--- victor-rpc-core/src/main/java/com/victor/vicrpc/proxy/ServiceProxy.java
+++ victor-rpc-core/src/main/java/com/victor/vicrpc/proxy/ServiceProxy.java
@@ -1,6 +1,7 @@
 package com.victor.vicrpc.proxy;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.victor.vicrpc.RpcApplication;
@@ -9,16 +10,22 @@ import com.victor.vicrpc.constant.RpcConstant;
 import com.victor.vicrpc.model.RpcRequest;
 import com.victor.vicrpc.model.RpcResponse;
 import com.victor.vicrpc.model.ServiceMetaInfo;
+import com.victor.vicrpc.protocol.*;
 import com.victor.vicrpc.registry.Registry;
 import com.victor.vicrpc.registry.RegistryFactory;
 import com.victor.vicrpc.serializer.JdkSerializer;
 import com.victor.vicrpc.serializer.Serializer;
 import com.victor.vicrpc.serializer.SerializerFactory;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /*
  *Author：Victor_htq
@@ -54,6 +61,7 @@ public class ServiceProxy implements InvocationHandler {
         // 指定序列化器
         Serializer serializer = SerializerFactory.getInstance(RpcApplication.getRpcConfig().getSerializer());
 
+        // 构造请求
         String serviceName = method.getDeclaringClass().getName();
         RpcRequest rpcRequest = RpcRequest.builder()
                 .serviceName(serviceName)
@@ -62,6 +70,7 @@ public class ServiceProxy implements InvocationHandler {
                 .args(args)
                 .build();
         try {
+            // 序列化
             byte[] bodyBytes = serializer.serialize(rpcRequest);
             // 从注册中心获取服务提供者请求地址
             RpcConfig rpcConfig = RpcApplication.getRpcConfig();
@@ -73,16 +82,63 @@ public class ServiceProxy implements InvocationHandler {
             if (CollUtil.isEmpty(serviceMetaInfoList)) {
                 throw new RuntimeException("暂无服务地址");
             }
-            ServiceMetaInfo selectedServiceMetaInfo1 = serviceMetaInfoList.get(0);
+            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
 
-        //     发送请求
-            try (HttpResponse httpResponse = HttpRequest.post(selectedServiceMetaInfo1.getServiceAddress())
-                         .body(bodyBytes)
-                         .execute()) {
-                byte[] result = httpResponse.bodyBytes();
-                RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
-                return rpcResponse.getData();
-            }
+            //     发送请求
+            //     try (HttpResponse httpResponse = HttpRequest.post(selectedServiceMetaInfo.getServiceAddress())
+            //                  .body(bodyBytes)
+            //                  .execute()) {
+            //         byte[] result = httpResponse.bodyBytes();
+            //         RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+            //         return rpcResponse.getData();
+            //     }
+            // } catch (IOException e) {
+            //     e.printStackTrace();
+            // }
+
+            // 发送TCP请求
+            Vertx vertx = Vertx.vertx();
+            NetClient netClient = vertx.createNetClient();
+            CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
+            netClient.connect(selectedServiceMetaInfo.getServicePort(),
+                    selectedServiceMetaInfo.getServiceHost(),
+                    result -> {
+                        if (result.succeeded()) {
+                            System.out.println("Connected to TCP server");
+                            NetSocket socket = result.result();
+                            // 构造消息
+                            ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+                            ProtocolMessage.Header header = new ProtocolMessage.Header();
+                            header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                            header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                            header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
+                            header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                            header.setRequestId(IdUtil.getSnowflakeNextId());
+                            protocolMessage.setHeader(header);
+                            protocolMessage.setBody(rpcRequest);
+                            // 编码请求
+                            try {
+                                Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+                                socket.write(encodeBuffer);
+                            } catch (IOException e) {
+                                throw new RuntimeException("协议消息编码错误");
+                            }
+
+                            socket.handler(buffer -> {
+                                try {
+                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage = (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                                } catch (IOException e) {
+                                    throw new RuntimeException("协议消息解码错误");
+                                }
+                            });
+                        } else {
+                            System.err.println("Failed to connect to TCP server");
+                        }
+                    });
+            RpcResponse rpcResponse = responseFuture.get();
+            // 关闭连接
+            netClient.close();
+            return rpcResponse.getData();
         } catch (IOException e) {
             e.printStackTrace();
         }
