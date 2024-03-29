94b2d38fe1b1596789437adc7a1cd4bcbaded62a
Victor <47047497+173716414@users.noreply.github.com>
2024-03-29 15:37:18 +0800

    请求处理器（服务提供者）

--- victor-rpc-core/src/main/java/com/victor/vicrpc/proxy/ServiceProxy.java
+++ victor-rpc-core/src/main/java/com/victor/vicrpc/proxy/ServiceProxy.java
@@ -29,6 +29,26 @@ import java.util.List;
  *Filename：ServiceProxy
  */
 public class ServiceProxy implements InvocationHandler {
+    /**
+     * 调用代理
+     * @param proxy the proxy instance that the method was invoked on
+     *
+     * @param method the {@code Method} instance corresponding to
+     * the interface method invoked on the proxy instance.  The declaring
+     * class of the {@code Method} object will be the interface that
+     * the method was declared in, which may be a superinterface of the
+     * proxy interface that the proxy class inherits the method through.
+     *
+     * @param args an array of objects containing the values of the
+     * arguments passed in the method invocation on the proxy instance,
+     * or {@code null} if interface method takes no arguments.
+     * Arguments of primitive types are wrapped in instances of the
+     * appropriate primitive wrapper class, such as
+     * {@code java.lang.Integer} or {@code java.lang.Boolean}.
+     *
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // 指定序列化器
